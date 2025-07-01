8f242dfb51cb00aee2a168f3d1af2d0e3c8e612a
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-01 09:55:29 +0200

    fetch data for 2 endpoints - the response differs so just print response length

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import requests
 import re
 
-def fix_link(link):
-    return re.sub(r'http://.*ogr\-oper\?', 'https://api.raporty.pse.pl/api/ogr-oper?', link)
+def fix_link(to_fix, endpoint):
+    return re.sub(r'http://.*?\?', f'{endpoint}?', to_fix)
 
-r = requests.get('https://api.raporty.pse.pl/api/ogr-oper')
-parsed = r.json()
-# todo - also parse/handle first response
-while 'nextLink' in parsed:
-    fixed_link = fix_link(parsed['nextLink'])
-    r = requests.get(fixed_link)
+def fetch_all(endpoint):
+    r = requests.get(endpoint)
     parsed = r.json()
-    dates = [item['from_dtime'] for item in parsed['value']]
-    dates = sorted(dates)
-    first = dates[0]
-    last = dates[-1]
-    print(f"from-to: {first} {last}")
+    # todo - also parse/handle first response
+    while 'nextLink' in parsed:
+        fixed_link = fix_link(parsed['nextLink'], endpoint)
+        r = requests.get(fixed_link)
+        parsed = r.json()
+        print(f'received response with len {len(r.content)}')
+
+endpoints = [
+    'https://api.raporty.pse.pl/api/ogr-oper',
+    'https://api.raporty.pse.pl/api/ogr-oper-head'
+]
+
+for endpoint in endpoints:
+    print(f"Fetching data from {endpoint}")
+    fetch_all(endpoint)
+    print("Done fetching data.")

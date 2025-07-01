9b9196d40e5eb97fc44f591cea07674bb47565e0
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-01 09:42:27 +0200

    fix nextLink

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import requests
+import re
 
+def fix_link(link):
+    return re.sub(r'http://.*ogr\-oper\?', 'https://api.raporty.pse.pl/api/ogr-oper?', link)
 
 r = requests.get('https://api.raporty.pse.pl/api/ogr-oper')
+parsed = r.json()
 
-print(r)
-print(r.json())
+fixed_link = fix_link(parsed['nextLink'])
+
+for i in range(100):
+    r = requests.get(fixed_link)
+    parsed = r.json()
+    dates = [item['from_dtime'] for item in parsed['value']]
+    dates = sorted(dates)
+    first = dates[0]
+    last = dates[-1]
+    print(f"from-to: {first} {last}")
+    fixed_link = fix_link(parsed['nextLink'])

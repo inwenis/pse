a304ea4be299e2e6e811dd0b8e1e71120838a748
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-02 12:44:13 +0200

    dump data to files

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ def fix_link(to_fix, endpoint):
 def fetch_all(endpoint):
     r = requests.get(endpoint)
     parsed = r.json()
+    count = 0
     # todo - also parse/handle first response
     while 'nextLink' in parsed:
         fixed_link = fix_link(parsed['nextLink'], endpoint)
         r = requests.get(fixed_link)
         parsed = r.json()
         print(f'received response with len {len(r.content)}')
-        # here you could dump the received json to a file, or send it to a database
+        filename = f"out/{endpoint.split('/')[-1]}_{count}.json"
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(r.text + '\n')
+        count += 1
 
 # if necessary not you could automate getting all endpoints from the main page
 # since we have code working for a hardcoded list of endpoints

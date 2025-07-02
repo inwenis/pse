782f5da640985c6387ebf390240c8805c9296ae5
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-02 12:51:40 +0200

    refactor - fetching data returns all data in a list

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import requests
 import re
 
-def fix_link(to_fix, endpoint):
+def fix_pagination_link(to_fix, endpoint):
     return re.sub(r'http://.*?\?', f'{endpoint}?', to_fix)
 
 def fetch_all(endpoint):
-    r = requests.get(endpoint)
-    parsed = r.json()
-    count = 0
-    # todo - also parse/handle first response
-    while 'nextLink' in parsed:
-        fixed_link = fix_link(parsed['nextLink'], endpoint)
-        r = requests.get(fixed_link)
-        parsed = r.json()
-        print(f'received response with len {len(r.content)}')
-        filename = f"out/{endpoint.split('/')[-1]}_{count}.json"
-        with open(filename, 'w', encoding='utf-8') as f:
-            f.write(r.text + '\n')
-        count += 1
+    all = []
+
+    current_link = endpoint
+
+    while True:
+        response = requests.get(current_link)
+        print(f'received response with len {len(response.content)}')
+        all.append(response.text)
+        parsed = response.json()
+        if 'nextLink' not in parsed:
+            break
+        else:
+            current_link = fix_pagination_link(parsed['nextLink'], endpoint)
+    return all
 
 # if necessary not you could automate getting all endpoints from the main page
 # since we have code working for a hardcoded list of endpoints

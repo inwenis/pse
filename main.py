bbd58bcb654150af56e611f8c81040b4db1684ea
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-04 10:42:23 +0200

    add all endpoints and move them to a separate file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import re
 import concurrent.futures
 import watcher
+from urls import urls
 
 def fix_pagination_link(to_fix, full_endpoint_url):
     return re.sub(r'http://.*?\?', f'{full_endpoint_url}?', to_fix)
@@ -10,18 +11,6 @@ def save_to_file(url, counter, content):
     endpoint = url.split('/')[-1]
     with open(f'out/{endpoint}_{counter}.json', 'w', encoding='utf-8') as f:
         f.write(content)
-
-# if necessary not you could automate getting all endpoints from the main page
-# since we have code working for a hardcoded list of endpoints
-urls = [
-    'https://api.raporty.pse.pl/api/ogr-oper',
-    'https://api.raporty.pse.pl/api/ogr-oper-head',
-    'https://api.raporty.pse.pl/api/it-omb-rbb',
-    'https://api.raporty.pse.pl/api/price-fcst',
-    'https://api.raporty.pse.pl/api/price-cost',
-    'https://api.raporty.pse.pl/api/pk5l-wp',
-    'https://api.raporty.pse.pl/api/unav-pk5l'
-]
 
 # I don't like using a global watcher
 # todo - stop using a global watcher in fetch_all()

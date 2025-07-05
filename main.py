9fa0cb0fe24f2c9f6062e1f33f0476caac7c0501
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-05 07:26:41 +0200

    scrape all urls

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def fetch_all(url):
 
     try:
         while True:
-            response = requests.get(current_link, timeout=2)
+            response = requests.get(current_link)
             watcher.report_scraped(url)
             all.append(response.text)
             parsed = response.json()
@@ -34,8 +34,6 @@ def fetch_all(url):
                 break
             else:
                 current_link = fix_pagination_link(parsed['nextLink'], url)
-        # todo - fix bug - it seems some urls are never marked as done
-        # i suspect it is because the watcher is not fully thread-safe
         watcher.report_scraped_all(url)
         return url, all
     except Exception:
@@ -49,11 +47,13 @@ os.makedirs('out', exist_ok=True) # Ensure the output directory exists before sa
 
 # I know little of parallel/concurrency in python so I'm not sure if this is the way to go
 # but it works
+results = []
 with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
     results = executor.map(fetch_all, urls)
-    for url, responses in results:
-        print(f'got {len(responses)} results for {url}')
-        for counter, content in enumerate(responses):
-            # todo - saving will fail if there is no 'out' directory
-            # saving to files could be handled in a separate class called FileSaver/Dumper/etc
-            save_to_file(url, counter, content)
+
+print(f'Fetched {len(urls)} urls')
+
+for url, responses in results:
+    print(f'got {len(responses)} results for {url}')
+    for counter, content in enumerate(responses):
+        save_to_file(url, counter, content)
